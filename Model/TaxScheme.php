a45912e33cf022aab1291c858d32ec397489a6bf
Graham Wharton <graham@gwharton.co.uk>
2025-02-11 21:08:28 +0000

    Add function to convert Tax Scheme to string. Used by autocustomergroup module.

--- Model/TaxScheme.php
+++ Model/TaxScheme.php
@@ -465,4 +465,12 @@ class TaxScheme implements TaxSchemeInterface
             $storeId
         );
     }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->getSchemeId();
+    }
 }
